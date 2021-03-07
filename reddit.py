d2a7275adc40bf2e1825686eb1ce971364cf38f9
Qinzheng(Ryan) Xu <63211787+xu000112-bit@users.noreply.github.com>
2021-03-07 12:36:51 -0600

    Add files via upload

--- reddit.py
+++ reddit.py
@@ -2,15 +2,17 @@ import pandas as pd
 import numpy as np
 import requests
 import praw
-import nltk
-nltk.download('vader_lexicon')
-from nltk.sentiment.vader import SentimentIntensityAnalyzer 
+import datetime
 
 client_ID = 'jRRw9hMnJBVXfQ'
 secret_key = 'nkFXjrzRGLMf54iwJOjsdvnWQDbyMQ'
 
 # User　Name: hg558636565
 # Pass Word: hg55863656365
+
+# https://api.pushshift.io/reddit/search/submission/?subreddit=wallstreetbets&sort=desc&sort_type=created_utc&after=1523588521&before=1523934121&size=1000
+# https://www.epochconverter.com/
+
 
 reddit = praw.Reddit(client_id = client_ID,
                      client_secret = secret_key,
@@ -23,22 +25,16 @@ posts = []
 ml_subreddit = reddit.subreddit('wallstreetbets')
 
 # limit could change to 'all'. means get all the post.
-for post in ml_subreddit.hot(limit=10):
-    # Sentiment Score
-    sid = SentimentIntensityAnalyzer()
-    ss = sid.polarity_scores(title)
-    for k in ss:
-        print('{0}:{1},'.format(k,ss[k]), end='')
-        if k == 'compound':
-            posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, post.created, ss[k]])
-posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created', 'sentiment'])
+for post in ml_subreddit.hot(limit=1000):
+    posts.append([post.title, post.score, post.id, post.subreddit, post.url, post.num_comments, post.selftext, datetime.datetime.fromtimestamp(post.created)])
+posts = pd.DataFrame(posts,columns=['title', 'score', 'id', 'subreddit', 'url', 'num_comments', 'body', 'created'])
 
 # Get Comments
 # id value is each post id. you could write a loop to download it.
-submission = reddit.submission(id="likmpp")
-submission.comments.replace_more(limit=0)
-comm = []
-for top_level_comment in submission.comments:
-    comm.append(top_level_comment.body)
+#submission = reddit.submission(id="likmpp")
+#submission.comments.replace_more(limit=0)
+#comm = []
+#for top_level_comment in submission.comments:
+#    comm.append(top_level_comment.body)
     
     
